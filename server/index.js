e5af925acd6b808bd55be73a63d7d9348edd9e4d
Sreejit Sadhukhan <sreejit.s501@gmail.com>
2025-04-13 03:50:10 +0530

    feat: update CORS configuration and clean up package.json scripts

--- server/index.js
+++ server/index.js
@@ -58,7 +58,14 @@ app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: [
+    process.env.CLIENT_URL,
+    'http://localhost:3000',
+    'https://medi-mantra-hacktonix.vercel.app/' // Add your frontend domain
+  ],
+  credentials: true
+}));
 
 // Database connection
 connectDB();
